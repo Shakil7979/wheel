43f2aa6e01747e7de30de21501cfaa8f46a226fc
Sadbin Shakil <sadbin7979@gmail.com>
2024-02-13 01:04:06 +0600

    update

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){ 
+
+	// Banner Carousel
+	$('.nftImage').owlCarousel({
+		items:1,
+		loop:true,
+		nav:true,
+		dots:false,
+        nav: true,
+        navText: ["<img src='../img/icon_left.svg'>","<img src='../img/icon_right.svg'>"]
+		// autoplay:true,
+	});
+
+
+
+
+
+});
